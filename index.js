f3099fbdb147760afee5ca75471b9431d8c4395a
Hitallo William <hitallo91@gmail.com>
2019-05-06 19:13:54 -0300

    iniciando com express

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
-const http = require('http');
+const express = require('express');
 
-let server = http.createServer((request, response) => {
+const app = express();
 
-    console.log("URL:", request.url);
-    console.log("METHOD:", request.method);
-    switch (request.url) {
-        case '/':
-            response.statusCode = 200;
-            response.setHeader('Content-type', 'text/html');
-            response.end("<h1>Você está acessando a pasta raiz</h1>");
-            break;
-        case '/users':
-            response.statusCode = 200;
-            response.setHeader('Content-type',"application/json");
-            response.end(JSON.stringify({
-                users:[{
-                    name:'Hitallo',
-                    email :'email@dominio.com',
-                    id : 1
-                }]
-            }));
-    }
-    response.end("OK");
+app.get("/", (request, response) => {
+    response.statusCode = 200;
+    response.setHeader('Content-type', 'text/html');
+    response.end("<h1>Você está acessando a pasta raiz</h1>");
+
 });
-server.listen(3000, '127.0.0.1', () => {
+app.get('/users', (request, response)=>{
+    response.statusCode = 200;
+    response.setHeader('Content-type', "application/json");
+    response.json({
+        users: [{
+            name: 'Hitallo',
+            email: 'email@dominio.com',
+            id: 1
+        }]
+    });
+}); 
+
+app.listen(3000, '127.0.0.1', () => {
     console.log("servidor rodando");
 });
