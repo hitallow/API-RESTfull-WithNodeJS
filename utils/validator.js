6c8b92c31eebf6e57821f0d60f553d2edf587d02
Hitallo William <hitallo91@gmail.com>
2019-05-24 05:43:16 -0300

    final commit

--- utils/validator.js
+++ utils/validator.js
@@ -2,7 +2,7 @@ module.exports = {
     user : (app, request, response)=>{
         request.assert("name", "O nome é obrigatório").notEmpty();
         request.assert("email", "O e-mail é obrigatório").notEmpty();
-        request.assert("e-mail",'o email deve ser válido').isEmail();
+        request.assert("e-mail",'O email deve ser válido').isEmail();
         let error = request.validationErrors();
         if(error){
             app.utils.error.send(error, request, response);
